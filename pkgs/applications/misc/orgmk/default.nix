1dcfa8b756478731f42349b9ce6f0b7981eaacc8
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-09-06 13:38:17 -0500

    orgmk

---
+++ pkgs/applications/misc/orgmk/default.nix
@@ -0,0 +1,28 @@
+{ stdenv, lib, fetchFromGitHub, pkgs, ... }:
+
+let
+  commonMeta = rec {
+    name = "orgmk";
+    version = "2022-01-20";
+  };
+in stdenv.mkDerivation {
+  name = "${commonMeta.name}_${commonMeta.version}";
+
+  buildInputs = with pkgs; [
+    emacs28NativeComp
+  ];
+  patches = [ ./params.patch ];
+  preBuild = "mkdir -p $out/bin";
+  src = fetchFromGitHub {
+    owner = "fniessen";
+    repo = "orgmk";
+    rev = "ba72326";
+    sha256 = "+2ldIlT7OFbYm+DWaP44eaiCacfQddjPRd1xasvE7/I=";
+  };
+  meta = {
+    homepage = "https://github.com/fniessen/orgmk";
+    platforms = [ "x86_64-linux" ];
+    license = with lib.licenses; [ gpl3 ];
+    maintainers = [ lib.maintainers.ProducerMatt ];
+  };
+}
