c2890eecefa7950238d57fff6d20ac7a9c10ff89
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-10-01 18:32:57 -0500

    yaml2nix

--- pkgs/tools/misc/yaml2nix/default.nix
+++ pkgs/tools/misc/yaml2nix/default.nix
@@ -8,10 +8,10 @@ rustPlatform.buildRustPackage rec {
     owner = "euank";
     repo = "yaml2nix";
     rev = "b220acf";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "KAWBoLjfk9jNVZ5tSODEq6r9A4I6eUlNwJPhCxkK+sQ=";
   };
 
-  cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+  cargoSha256 = "FZ9GlNyFX+15sza9sTbHqLvkVpu1oBYzo0ydCptyZyc=";
 
   meta = with lib; {
     description = "A command-line tool to convert yaml into a nix expression";
