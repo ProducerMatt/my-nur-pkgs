48dca16213cfad3742d1d7d81fc2339cfb871e3f
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-09-02 23:17:20 -0500

    cosmo: broken test 4

--- pkgs/cosmo/default.nix
+++ pkgs/cosmo/default.nix
@@ -224,6 +224,7 @@ stdenv.mkDerivation {
       rm test/libc/calls/ioctl_test.c
       rm test/libc/calls/sched_getaffinity_test.c
       rm test/libc/sock/socket_test.c
+      rm test/libc/calls/sched_getaffinity_test.c
 
       # fails
       rm test/libc/stdio/posix_spawn_test.c
